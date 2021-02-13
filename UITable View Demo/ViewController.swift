1dc18f340220ec7eca25bb088673e3f3f7df9d5e
Subba <Subba@gmail.com>
2021-02-13 13:03:55 -0600

    Initial Commit

---
+++ UITable View Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UITable View Demo
+//
+//  Created by SubbaLakshmi on 2/13/21.
+//  Copyright © 2021 SubbaLakshmi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

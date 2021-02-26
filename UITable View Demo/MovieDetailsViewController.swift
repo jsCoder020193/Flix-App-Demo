c2bf05183e9899095821b52fd394d57f75023878
Subba <Subba@gmail.com>
2021-02-26 02:11:26 -0600

    Flix Part 2

---
+++ UITable View Demo/MovieDetailsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MovieDetailsViewController.swift
+//  UITable View Demo
+//
+//  Created by SubbaLakshmi on 2/26/21.
+//  Copyright © 2021 SubbaLakshmi. All rights reserved.
+//
+
+import UIKit
+
+class MovieDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ UITable View Demo/MovieGridCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCollectionViewCell.swift
+//  UITable View Demo
+//
+//  Created by SubbaLakshmi on 2/26/21.
+//  Copyright © 2021 SubbaLakshmi. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCollectionViewCell: UICollectionViewCell {
+    
+}
